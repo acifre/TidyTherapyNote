ca1deb66924f06da413a02c30868674f255254ca
acifre <anthonybcifre@gmail.com>
2023-05-06 11:45:45 -0500

    Initial Commit

---
+++ TidyTherapy/TidyTherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidyTherapyApp.swift
+//  TidyTherapy
+//
+//  Created by Anthony Cifre on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TidyTherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
