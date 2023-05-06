eb5d73106eba0a1d727d52354d8bf716d3a6bba0
acifre <anthonybcifre@gmail.com>
2023-05-06 15:50:32 -0500

    Redo contentview format

--- TidyTherapy/ContentView.swift
+++ TidyTherapy/ContentView.swift
@@ -11,47 +11,63 @@ struct ContentView: View {
     @State private var patientName = ""
     @State private var guardianName = ""
     @State var chatRole = "I want you to act as a psychology therapy note cleaner and editor. I will provide you with a therapy note for a patient. You should use your knowledge of cognitive behavioral therapy, acceptance-committment therapy, meditation techniques, mindfulness practices, and other therapeutic methods in order to edit the note to make it sound more professional and more concise (but not too concise). My first request is "
-    @State var text = ""
+    @State var dirtyNote = ""
+    @State var currentRequest = ""
+    @State var cleanedNote = ""
     @State var notes = [String]()
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach(notes, id: \.self) {
-                        string in HStack {
-                            Text(string)
-                        }
-                    }
-                    Spacer()
-                    VStack {
-                        HStack {
-                            TextField("Patient's Name", text: $patientName)
-                                .textFieldStyle(.roundedBorder)
-                                .padding()
-                            TextField("Guardian's Name", text: $guardianName)
-                                .textFieldStyle(.roundedBorder)
-                                .padding()
-                        }
-                        HStack {
-//                            Image("therapy")
-//                                .resizable()
-//                                .frame(width: 40, height: 40)
-                            TextField("Enter your note text", text: $text, axis: .vertical)
-                                .textFieldStyle(.roundedBorder)
-                                .padding()
-
-                        }
-                        Button("Send") {
-                            send()
-                        }
-                        .frame(width: 100, height: 40).background(.blue)
-                        .foregroundColor(.white)
-                        .font(.title3)
-                        .cornerRadius(12)
-                    }
+            VStack {
+                Text("Dirty Note")
+                    .padding(1)
+                ScrollView {
+                    Text(currentRequest)
+                        .padding()
                 }
-                .padding(.horizontal)
-            .navigationTitle("TidyTherapyNote")
+                .frame(maxWidth: .infinity)
+                .border(.black)
+                .cornerRadius(5)
+                .padding()
+                
+                Text("Cleaned Note")
+                    .padding(1)
+                ScrollView {
+                    Text(cleanedNote)
+                        .padding()
+                }
+                .frame(maxWidth: .infinity)
+                .border(.black)
+                .cornerRadius(5)
+                .padding()
+                
+                VStack {
+                    HStack {
+                        TextField("Patient's Name", text: $patientName)
+                            .textFieldStyle(.roundedBorder)
+                            .border(.black)
+                            .padding()
+                        TextField("Guardian's Name", text: $guardianName)
+                            .textFieldStyle(.roundedBorder)
+                            .border(.black)
+                            .padding()
+                    }
+                    HStack {
+                        TextField("Enter your note text", text: $dirtyNote, axis: .vertical)
+                            .textFieldStyle(.roundedBorder)
+                            .border(.blue)
+                            .padding()
+
+                    }
+                    Button("Send") {
+                        send()
+                    }
+                    .frame(width: 100, height: 40).background(.blue)
+                    .foregroundColor(.white)
+                    .font(.title3)
+                    .cornerRadius(12)
+                }
+                    .padding(.horizontal)
+                .navigationTitle("TidyTherapyNote")
             }
         }
         .onAppear {
@@ -59,16 +75,18 @@ struct ContentView: View {
         }
     }
     func send() {
-        guard !text.trimmingCharacters(in: .whitespaces).isEmpty
+        guard !dirtyNote.trimmingCharacters(in: .whitespaces).isEmpty
         else {
             return
         }
-        notes.append("Me: \n\n\(text.trimmingCharacters(in: .whitespaces)) \n")
-        viewmodel.send(text: chatRole + text.replacingOccurrences(of: patientName, with: "Anthony")) {
+        notes.append("Me: \n\n\(dirtyNote.trimmingCharacters(in: .whitespaces)) \n")
+        self.currentRequest = dirtyNote.trimmingCharacters(in: .whitespaces)
+        viewmodel.send(text: chatRole + dirtyNote.replacingOccurrences(of: patientName, with: "Anthony", options: .caseInsensitive)) {
             result in print(result)
             self.notes.append("Cleaned Note: \n" + result)
+            self.cleanedNote = result.trimmingCharacters(in: .whitespacesAndNewlines)
         }
-        self.text = ""
+        self.dirtyNote = ""
         self.patientName = ""
     }
 }
